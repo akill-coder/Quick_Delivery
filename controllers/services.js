40304583f94c5422d8ee30ade93d8086db7f3c92
Akill Coder <66215139+akill-coder@users.noreply.github.com>
2020-12-21 19:22:34 +0530

    services completed, resolved users bugs

--- controllers/services.js
+++ controllers/services.js
@@ -2,6 +2,7 @@ const servicesModel = require('../models/services.js');
 const serviceModel = require('../models/services.js');
 
 
+
 exports.create = async (req, res) => {
 
     try {
@@ -89,3 +90,118 @@ exports.getAll = async (req,res) => {
 
 };
 
+
+exports.getById = async (req, res) => {
+
+    try {
+
+        const data = await servicesModel.findOne({serviceId : req.params.serviceId});
+
+        if(data != null){
+
+            res.status(200).json(
+                {
+                    status: "success",
+                    data: data,
+
+                }
+            );
+        }else{
+            res.status(400).json(
+                {
+                    status: "success",
+                    msg: "Please Check the serviceId",
+
+                }
+            );
+
+        }
+        
+    } catch (error) {
+
+        res.status(404).json({
+            status : 'error',
+            msg : error
+        });
+
+        
+    }
+
+
+};
+
+
+exports.update = async (req,res) => {
+
+    try {
+
+
+
+
+        if( (await servicesModel.find({serviceId : req.body.serviceId})).length == 0 ){
+
+            res.status(400).json({
+                status : 'error',
+                msg : 'check the serviceID'
+            });
+
+        }
+        else if( (await servicesModel.find({serviceName : req.body.serviceName})).length != 0 ){
+
+            res.status(400).json(
+                { 
+                    status : 'error',
+                    msg : 'Service Name Already Exists'
+                }
+            );
+
+        }       
+        else{
+
+            
+
+            const updatedData = await servicesModel.findOneAndUpdate(
+                {
+                    serviceId: req.body.serviceId
+                },
+                {
+                    $set : { 
+                    serviceName : req.body.serviceName,
+                    description : req.body.description,
+                    parcelSize : req.body.parcelCharges,
+                    discount : req.body.discount,
+                    parcelCharges : req.body.parcelCharges,
+                    updateAt : Date()
+                    }
+
+                },
+                {
+                    new: true, //to return new doc back
+                    runValidators: true, //to run the validators which specified in the model
+                }
+
+            );
+
+            console.log(updatedData);
+
+
+            res.status(200).json(
+
+                {
+                    status: "success",                
+                    message: "Service Data updated successfully"                
+                }
+
+            );
+
+        }
+        
+    } catch (error) {
+        
+        res.status(404).json({
+            status : 'error',
+            msg : error.message
+        });
+
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -6,14 +6,18 @@ const controllerService = require('../controllers/services.js');
 
 const route = express.Router();
 
+// user routes
 route.post('/registerUser', controllerUsers.register);
-
 route.post('/loginUser', controllerUsers.login);
 
+// services routes
 route.post('/createService', controllerService.create);
-
 route.get('/getAllServices', controllerService.getAll);
+route.get('/getServiceData/:serviceId', controllerService.getById);
+route.put('/updateService', controllerService.update);
 
+
+// invalid routes
 route.all('*', errHandler.invalidPath);
 
 
